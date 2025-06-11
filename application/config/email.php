51a5323ffa338fda27273897fe353a430ee4241f
Alan Silva <116116386+AlanSilva666@users.noreply.github.com>
2025-06-11 13:33:47 -0300

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['protocol']    = 'smtp';
 $config['smtp_host']   = 'smtps.uhserver.com';
 $config['smtp_user']   = 'vendas@ibcel.com.br';
-$config['smtp_pass']   = 'Sib142vd2025@'; // Lembre-se de colocar a senha de aplicativo real
+$config['smtp_pass']   = ''; // Lembre-se de colocar a senha de aplicativo real
 $config['smtp_port']   = 465;
 $config['smtp_crypto'] = 'ssl'; // ALTERADO: Para a porta 465, use 'ssl'
 $config['mailtype']    = 'html';
