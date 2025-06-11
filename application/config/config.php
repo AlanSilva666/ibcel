b720edba217011cbbf866e06b41c400d02c9170a
Alan Silva <116116386+AlanSilva666@users.noreply.github.com>
2025-06-11 07:57:25 -0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'https://ibcel.com.br';
+// $config['base_url'] = 'https://localhost/ibcel';
+$config['base_url'] = 'https://ibcel.com.br/';
 
 /*
 |--------------------------------------------------------------------------
@@ -360,7 +361,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = APPPATH . 'cache/session_data'; // Exemplo comum
+$config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
@@ -380,12 +381,16 @@ $config['sess_regenerate_destroy'] = FALSE;
 |       'cookie_httponly') will also affect sessions.
 |
 */
-$config['cookie_prefix']	= '';
-$config['cookie_domain']	= '';
-$config['cookie_path']		= '/';
-$config['cookie_secure']	= FALSE;
-$config['cookie_httponly'] 	= FALSE;
-
+// $config['cookie_prefix']	= '';
+// $config['cookie_domain']	= '';
+// $config['cookie_path']		= '/';
+// $config['cookie_secure']	= FALSE;
+// $config['cookie_httponly'] 	= FALSE;
+$config['cookie_prefix']    = '';
+$config['cookie_domain']    = '.ibcel.com.br'; // Defina o domínio do seu site
+$config['cookie_path']      = '/';
+$config['cookie_secure']    = TRUE; // Força cookies apenas em conexões seguras
+$config['cookie_httponly']  = TRUE; // Impede acesso via JavaScript, aumentando a segurança
 /*
 |--------------------------------------------------------------------------
 | Standardize newlines
