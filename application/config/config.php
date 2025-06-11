f2a013c9f3565e63a937fa4703abf7bfd60d230b
Alan Silva <116116386+AlanSilva666@users.noreply.github.com>
2025-06-11 08:15:28 -0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -18,7 +18,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'https://localhost/ibcel';
-$config['base_url'] = 'https://ibcel.com.br/';
+// $config['base_url'] = 'https://ibcel.com.br/';
+// application/config/config.php
+$config['base_url'] = 'https://ibcel.com.br/web/';
 
 /*
 |--------------------------------------------------------------------------
@@ -361,7 +363,10 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+// $config['sess_save_path'] = NULL; // Tente NULL primeiro, se a pasta estiver gravável
+// $config['sess_save_path'] = APPPATH . '../sessions';
+// Exemplo: se o caminho absoluto for /home/ibcel/ibcel.com.br/web/application/cache/session_data
+$config['sess_save_path'] = '/home/ibcel/ibcel.com.br/web/application/cache/session_data';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
